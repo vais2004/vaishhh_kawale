d81c1b4740088af40b2d1bf5786cc6d67b8c8046
41-Xll-A-vaishnavi kawale <114976538+vais2004@users.noreply.github.com>
2025-10-22 17:04:40 +0000

    update feat: resume of one page only

--- src/App.js
+++ src/App.js
@@ -57,8 +57,8 @@ export default function App() {
               <h1 className="display-4">I'm Vaishnavi Kawale</h1>
               <p className="lead text-muted">Full Stack Developer</p>
               <a
-                href={`${process.env.PUBLIC_URL}/assets/Vaishnavi_Kawale_Resume.pdf`}
-                download="Vaishnavi_Kawale_Resume.pdf"
+                href={`${process.env.PUBLIC_URL}/assets/vaishnavi_h_kawale_resume.pdf`}
+                download="vaishnavi_h_kawale_resume.pdf"
                 className="btn btn-outline-primary mb-2"
               >
                 Download Resume
